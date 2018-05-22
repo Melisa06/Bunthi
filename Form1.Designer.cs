ce45b72b178d5b4835144c19951590b6a29b5ef8
Melisa06 <35781174+Melisa06@users.noreply.github.com>
2018-05-22 15:17:56 -0500

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Bunthi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBuscar_Puertos = new System.Windows.Forms.Button();
+            this.cmbPuertos = new System.Windows.Forms.ComboBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.btnArchivos = new System.Windows.Forms.Button();
+            this.lblDatoRecibido = new System.Windows.Forms.Label();
+            this.txtDatoRecibido = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Temperatura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Luminosidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tTime = new System.Windows.Forms.Timer(this.components);
+            this.spPuertoSerial = new System.IO.Ports.SerialPort(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtBrowse = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBuscar_Puertos
+            // 
+            this.btnBuscar_Puertos.Location = new System.Drawing.Point(12, 12);
+            this.btnBuscar_Puertos.Name = "btnBuscar_Puertos";
+            this.btnBuscar_Puertos.Size = new System.Drawing.Size(151, 31);
+            this.btnBuscar_Puertos.TabIndex = 0;
+            this.btnBuscar_Puertos.Text = "BUSCAR PUERTOS";
+            this.btnBuscar_Puertos.UseVisualStyleBackColor = true;
+            this.btnBuscar_Puertos.Click += new System.EventHandler(this.btnBuscar_Puertos_Click);
+            // 
+            // cmbPuertos
+            // 
+            this.cmbPuertos.FormattingEnabled = true;
+            this.cmbPuertos.Location = new System.Drawing.Point(180, 17);
+            this.cmbPuertos.Name = "cmbPuertos";
+            this.cmbPuertos.Size = new System.Drawing.Size(121, 21);
+            this.cmbPuertos.TabIndex = 1;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(323, 12);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(104, 31);
+            this.btnConectar.TabIndex = 2;
+            this.btnConectar.Text = "CONECTAR";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(451, 25);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(42, 13);
+            this.lblTiempo.TabIndex = 3;
+            this.lblTiempo.Text = "Tiempo";
+            // 
+            // btnArchivos
+            // 
+            this.btnArchivos.Location = new System.Drawing.Point(12, 74);
+            this.btnArchivos.Name = "btnArchivos";
+            this.btnArchivos.Size = new System.Drawing.Size(151, 32);
+            this.btnArchivos.TabIndex = 4;
+            this.btnArchivos.Text = "GUARDAR ARCHIVO";
+            this.btnArchivos.UseVisualStyleBackColor = true;
+            this.btnArchivos.Click += new System.EventHandler(this.btnArchivos_Click);
+            // 
+            // lblDatoRecibido
+            // 
+            this.lblDatoRecibido.AutoSize = true;
+            this.lblDatoRecibido.Location = new System.Drawing.Point(13, 133);
+            this.lblDatoRecibido.Name = "lblDatoRecibido";
+            this.lblDatoRecibido.Size = new System.Drawing.Size(91, 13);
+            this.lblDatoRecibido.TabIndex = 6;
+            this.lblDatoRecibido.Text = "DATO RECIBIDO";
+            // 
+            // txtDatoRecibido
+            // 
+            this.txtDatoRecibido.Location = new System.Drawing.Point(180, 125);
+            this.txtDatoRecibido.Name = "txtDatoRecibido";
+            this.txtDatoRecibido.Size = new System.Drawing.Size(121, 20);
+            this.txtDatoRecibido.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Temperatura,
+            this.Luminosidad,
+            this.Fecha,
+            this.Hora});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 214);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(446, 173);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Temperatura
+            // 
+            this.Temperatura.HeaderText = "Temperatura";
+            this.Temperatura.Name = "Temperatura";
+            // 
+            // Luminosidad
+            // 
+            this.Luminosidad.HeaderText = "Luminosidad";
+            this.Luminosidad.Name = "Luminosidad";
+            // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "Fecha";
+            this.Fecha.Name = "Fecha";
+            // 
+            // Hora
+            // 
+            this.Hora.HeaderText = "Hora";
+            this.Hora.Name = "Hora";
+            // 
+            // tTime
+            // 
+            this.tTime.Enabled = true;
+            this.tTime.Tick += new System.EventHandler(this.tTime_Tick);
+            // 
+            // spPuertoSerial
+            // 
+            this.spPuertoSerial.RtsEnable = true;
+            this.spPuertoSerial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.spPuertoSerial_DataReceived);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "Bunthi";
+            this.saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files |*.*";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // txtBrowse
+            // 
+            this.txtBrowse.Location = new System.Drawing.Point(190, 81);
+            this.txtBrowse.Name = "txtBrowse";
+            this.txtBrowse.Size = new System.Drawing.Size(111, 20);
+            this.txtBrowse.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 411);
+            this.Controls.Add(this.txtBrowse);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtDatoRecibido);
+            this.Controls.Add(this.lblDatoRecibido);
+            this.Controls.Add(this.btnArchivos);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.cmbPuertos);
+            this.Controls.Add(this.btnBuscar_Puertos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscar_Puertos;
+        private System.Windows.Forms.ComboBox cmbPuertos;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Button btnArchivos;
+        private System.Windows.Forms.Label lblDatoRecibido;
+        private System.Windows.Forms.TextBox txtDatoRecibido;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Temperatura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Luminosidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hora;
+        private System.Windows.Forms.Timer tTime;
+        private System.IO.Ports.SerialPort spPuertoSerial;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtBrowse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,359 @@
+﻿//Librerias utilizadas 
+using System;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Drawing;
+using System.Linq;
+
+namespace Bunthi
+{
+    public partial class Form1 : Form
+    {
+        //Este es un list privado para que siempre tenga los valores sea cual sea la acción que se realice
+        //O al menos en esta clase
+        private List<Datos> datosDeTxt = new List<Datos>();
+        //Variable para dar acceso al form 
+        private delegate void DelegadoAcceso(string accion);
+        //Variables para la lectura de datos
+        private string strBufferIn;
+        private string strBufferOut;
+
+        //Esta es para tomar la fecha, hora al momento de conectarse al puerto
+        private DateTime fecha = DateTime.Now;
+        //Es el contador que vamos a usar por mientras
+        //private int aumentoTiempo = 0;
+        //Es el contador que vamos a usar por mientras
+        private TimeSpan aumentoTiempo = new TimeSpan(0, 1, 0);
+        //Variable para abrir ventana de busqueda de archivos 
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        private string ruta;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //gets & sets 
+        public class Datos
+        {
+
+            public string Temp { get; set; }
+            public string Lum { get; set; }
+            public string Fecha { get; set; }
+            public string Hora { get; set; }
+        }
+        /// <summary>
+        /// Esta función es para validar si entra o no en el rango de tiempo
+        /// </summary>
+        /// <param name="aumento">es el aumento en minutos que se tendra que sumar a la hora anterior</param>
+        public bool MandarDatos(TimeSpan aumento)
+        {
+
+            //Se usa para validar el minuto actual 
+            System.Diagnostics.Debug.WriteLine("Minuto anterior: " + fecha.ToString("mm") + "  Minuto actual: " + DateTime.Now.ToString("mm"));
+
+            if ((fecha + aumentoTiempo).ToString("mm") == DateTime.Now.ToString("mm"))
+            {
+                fecha = fecha + aumentoTiempo;
+                //MessageBox.Show("aqui voy");
+                return true;
+            }
+
+            else
+                return false;
+
+        }
+
+        //---------------------------Metodo para leer dato e insertar en un .txt-------------
+
+        public void LeerDatos(Datos datoRecibido)
+        {
+            ruta = txtBrowse.Text;
+            //Primero se agrega el dato a la lista
+            datosDeTxt.Add(datoRecibido);
+
+            //Luego la lista se cambia a el sentido contrario, esto para que el ultimo dato ingresado salga al inicio del txt
+            datosDeTxt.Reverse();
+
+            //Iniciamos el StreamWriter que es el motor para crear el archivo, se especifica la ruta donde se almacenara
+            // System.IO.StreamWriter stre = new System.IO.StreamWriter(Server.MapPath("C:/Datos/datos.txt") + "datos.txt", true); 
+            StreamWriter sw = new StreamWriter(ruta);
+
+            //Creamos el encabezado
+            sw.WriteLine("+-------------+-------------+------------+-------------+");
+            sw.WriteLine("|TEMPERATURA  | LUMINOSIDAD |    FECHA   |    HORA     |");
+            sw.WriteLine("+-------------+-------------+------------+-------------+");
+
+            //Creamos un foreach para recorrer la lista inversa y separar los datos del mismo
+            foreach (var dato in datosDeTxt)
+            {
+                string espaciado = "";
+                //Cada valor separado se agrega a una linea en el archivo
+
+                for (int i = 0; i < 13 - (dato.Temp.Length + 1); i++)
+                    espaciado = espaciado + " ";
+
+                sw.WriteLine("|" + espaciado + dato.Temp + " |" + espaciado + dato.Lum + "," + "|" + dato.Fecha + "," + "|" + dato.Hora + "|");
+            }
+            sw.WriteLine("+-------------+-------------+-------------+-------------+");
+
+            //Cerramos el motor para que se guarden los cambios en el archivo
+            sw.Close();
+
+            //Volvemos a invertir la lista para que el siguiente dato sigua la linea de la lista correctamente
+            datosDeTxt.Reverse();
+
+        }//cierra metodo leer dato 
+
+        //metodo para dar acceso al form
+        private void AccesoForm(string accion)
+        {
+            //--------------------- tener  acceso a los datos que se ingresan desde el puerto serial
+
+            //El numero 10 es provisional
+
+            if (MandarDatos(new TimeSpan(0, 1, 0)))
+            {
+                try
+                {
+                    strBufferIn = accion;
+                    //Aqui es donde se hara la separación de datos
+
+                    System.Diagnostics.Debug.WriteLine(accion);
+
+                    string  temp, lum;
+                    //Se hace un array el cual se separa por la coma para eso funciona la herramienta split 
+                    //(Puede ser cualquier parametro desde una palabra hasta caracteres especiales en un orden especifico)
+                    string[] subStrings = accion.Split(',');
+                    temp = subStrings[0];
+                    lum = subStrings[1];
+
+
+
+                    txtDatoRecibido.Text = accion;
+                    spPuertoSerial.DiscardInBuffer();
+
+                    Datos datosNuevos = new Datos();
+                    datosNuevos.Temp = temp;
+                    datosNuevos.Lum = lum;
+                    datosNuevos.Fecha = DateTime.Now.ToShortDateString();
+                    datosNuevos.Hora = DateTime.Now.ToLongTimeString();
+
+                    LeerDatos(datosNuevos);
+                    //Este metodo ingresa datos al gridView y lo agrega, no se puede poner un objeto, se tiene que poner separado por comas
+                    //Asi separamos el objeto actual en los elementos separados
+                    dataGridView1.Rows.Add(datosNuevos.Temp, datosNuevos.Lum, datosNuevos.Fecha, datosNuevos.Hora);
+                    //spPuertoSerial.DiscardOutBuffer();
+
+                    //System.Diagnostics.Debug.WriteLine("1");
+
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message.ToString());
+                }
+            }
+        }
+
+        //Metodo para estar guardando los datos 
+        private void AccesoInterrupcion(string accion)
+        {
+            try
+            {
+                DelegadoAcceso var_DA;
+                var_DA = new DelegadoAcceso(AccesoForm);
+
+
+                object[] arg = { accion };
+
+                base.Invoke(var_DA, arg);
+            }
+
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("No toma el valor o supera el buffer asignado");
+            }
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            strBufferIn = "";
+            strBufferOut = "";
+            btnConectar.Enabled = false;
+        }
+
+        private void btnBuscar_Puertos_Click(object sender, EventArgs e)
+        {
+            // en un arreglo se guardarn los datos disponobles 
+            string[] PuertosDisponibles = SerialPort.GetPortNames();
+            cmbPuertos.Items.Clear();
+
+            foreach (string puerto in PuertosDisponibles)
+            {
+                // en el combo box puertos 
+                // se guardaran los puertos disponibles 
+                cmbPuertos.Items.Add(puerto);
+
+            }
+            //si el cmbPuertos es mayor que 0 se mostrará  el siguiente mensaje 
+            if (cmbPuertos.Items.Count > 0)
+            {
+                cmbPuertos.SelectedIndex = 0;
+                //-------------------------------Mensaje para seleccionar el puerto 
+                // Esto nos indica que el metodo para encontrar los seriales esta funcionando 
+                MessageBox.Show("Seleccionar el puerto");
+
+                btnConectar.Enabled = true;
+                //------------------------------------------------------------------------------
+            }
+            //si no se encuentra ningun puerto pasara al siguiente else 
+            else
+            {
+                MessageBox.Show("Ningun puerto seleccionado");
+                cmbPuertos.Items.Clear();
+                cmbPuertos.Text = "                       ";
+                strBufferIn = "";
+                strBufferOut = "";
+                btnConectar.Enabled = false;
+            }
+        }
+        //-------
+        /*
+    *En el botón conectar le pasaremos los 
+    * parametros que necesita para recibir los datos 
+    * 
+    */
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            // La función y el metodo para conectra con el serial
+            try
+            {
+                if (btnConectar.Text == "CONECTAR")
+                {
+                    System.Diagnostics.Debug.WriteLine("1");
+
+                    //datos para poder conectar con el serial 
+                    spPuertoSerial.BaudRate = 9600;
+                    spPuertoSerial.DataBits = 8;
+                    spPuertoSerial.Parity = Parity.None;
+                    spPuertoSerial.StopBits = StopBits.One;
+                    spPuertoSerial.Encoding = Encoding.ASCII;
+                    //spPuertoSerial.Handshake = Handshake.None;// para que sirve?
+                    // No lo se eso lo puse del video 
+                    //este es otro emulador 
+
+                    spPuertoSerial.PortName = cmbPuertos.Text;
+
+                    try
+                    {
+                        //Línea de código para abrir el puerto serial 
+                        spPuertoSerial.Open();
+                        spPuertoSerial.DiscardOutBuffer();
+
+                        strBufferIn = "";
+                        strBufferOut = "1";
+                        /*
+                         * Cuando se conecte al serial 
+                         * este mandara un dato de 
+                         * para concer que si esta concetado 
+                         * 
+                         */
+                        spPuertoSerial.DiscardInBuffer();
+                        spPuertoSerial.DiscardOutBuffer();
+                        AccesoInterrupcion(spPuertoSerial.ReadExisting());
+                        strBufferIn = "";
+                        strBufferOut = "";
+                        btnConectar.Text = "DESCONECTAR";
+
+                        //Lo que se hace es tomar toda la fecha, dia/mes/año hh:mm:ss por cualquier cosa que quieras hacer despues
+                        //Luego lo que hago es separar esa fecha en solo los minutos para comenzar a hacer el contador 
+                        fecha = DateTime.Now;
+                        // aumentoTiempo = Int32.Parse(fecha.ToString("mm"));
+                    }
+                    catch (Exception EXC)
+                    {
+                        MessageBox.Show(EXC.Message.ToString());
+                    }
+                }
+                //EL else if es para cambiar el botn de conectar por las palabras "desconectar"
+                else if (btnConectar.Text == "DESCONECTAR")
+                {
+                    spPuertoSerial.Close();
+                    btnConectar.Text = "CONECTAR";
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message.ToString());
+            }
+        }
+        //--------------
+
+        private void spPuertoSerial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                Thread.Sleep(9000);
+                string dato = spPuertoSerial.ReadExisting();
+                AccesoInterrupcion(dato);
+                System.Diagnostics.Debug.WriteLine(strBufferIn);
+            }
+
+            catch (Exception ec)
+            {
+                MessageBox.Show(ec.Message.ToString());
+            }
+        }
+        //--------------
+        private void tTime_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                lblTiempo.Text = DateTime.Now.ToLongTimeString();
+
+                tTime.Start();
+                if ((fecha + aumentoTiempo).ToString("mm") == DateTime.Now.ToString("mm"))
+                {
+                    //spPuertoSerial.DiscardInBuffer();
+                    //spPuertoSerial.DiscardOutBuffer();
+                    spPuertoSerial.Write("1");
+                    Thread.Sleep(9000);
+                    System.Diagnostics.Debug.WriteLine(strBufferOut);
+                    spPuertoSerial.DiscardOutBuffer();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Truena por el minuto" + ex);
+            }
+        }
+        //--------------
+
+      
+
+        private void btnArchivos_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+             txtBrowse.Text = saveFileDialog1.FileName;
+        
+        }
+
+      
+        //--------------
+
+
+
+    }
+}
